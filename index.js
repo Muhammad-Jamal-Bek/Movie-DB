6c1c1ebc65b3c984dc06ac7d00ee250dbdb500e8
muhammad-jamal-bek <muhammad-jamal-bek@hotmail.com>
2021-06-14 14:06:59 +0300

    Step 4

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.get('/test', (req, res) => {
     const response={
         status:200, message:"OK"
     };
-    res.send(response)
+    res.send(response);
 })
 
 app.get('/time', (req, res) => {
@@ -14,9 +14,39 @@ app.get('/time', (req, res) => {
     const response={
         status:200, message: date.getHours()+":"+date.getMinutes()
     };
-    res.send(response)
+    res.send(response);
 })
 
+app.get('/hello', (req, res) => {
+    const response={
+        status:200, message: "Hello!"
+    };
+    res.send(response);
+})
+
+app.get('/hello/:id', (req, res) => {
+    const response={
+        status:200, message: "Hello "+req.params.id+"!"
+    };
+    res.send(response);
+})
+app.get('/search', (req, res) => {
+    if (req.query.s!=null)
+    {
+    const response= {
+
+        status:200, message:"ok ",data:req.query.s
+    };
+    res.send(response);
+}
+else {
+    const response= {
+    status:500, error:true, message:"you have to provide a search"
+};
+
+res.send(response);
+}
+})
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log('server listening on port http://localhost:${port}')
 })
