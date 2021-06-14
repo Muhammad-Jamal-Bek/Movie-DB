6ab1e9dd62fbf6f19c072ea607cb8d3ffbf62eb7
muhammad-jamal-bek <muhammad-jamal-bek@hotmail.com>
2021-06-14 16:22:35 +0300

    Step 5

--- index.js
+++ index.js
@@ -68,6 +68,46 @@ app.get('/movies/read', (req, res) => {
     };
     res.send(response);
 })
+app.get('/movies/read/by-date', (req, res) => {
+    var sorted_year=movies.sort(function(a, b) {
+        var keyA = new Date(a.year),
+          keyB = new Date(b.year);
+        if (keyA < keyB) return -1;
+        if (keyA > keyB) return 1;
+        return 0;
+      })
+    const response={
+        status:200, data: sorted_year
+    };
+    res.send(response);
+})
+
+app.get('/movies/read/by-rating', (req, res) => {
+    var sorted_year=movies.sort(function(a, b) {
+        var keyA = new Date(a.rating),
+          keyB = new Date(b.rating);
+        if (keyA > keyB) return -1;
+        if (keyA < keyB) return 1;
+        return 0;
+      })
+    const response={
+        status:200, data: sorted_year
+    };
+    res.send(response);
+})
+
+
+app.get('/movies/read/by-title', (req, res) => {
+    var sorted_year=movies.sort(function(a,b) {
+        if (a.title<b.title) return -1;
+        if (a.title>b.title) return 1;
+        return 0;
+    });
+    const response={
+        status:200, data: sorted_year
+    };
+    res.send(response);
+})
 
 app.get('/movies/update', (req, res) => {
     const response={
