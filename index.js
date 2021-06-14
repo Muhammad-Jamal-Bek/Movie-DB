b8105ed311aa5f654cabb45a533cc399ca4942b7
muhammad-jamal-bek <muhammad-jamal-bek@hotmail.com>
2021-06-14 15:48:13 +0300

    Step 5

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const movies = [
+    { title: 'Jaws', year: 1975, rating: 8 },
+    { title: 'Avatar', year: 2009, rating: 7.8 },
+    { title: 'Brazil', year: 1985, rating: 8 },
+    { title: 'الإرهاب والكباب‎', year: 1992, rating: 6.2 }
+]
 
 app.get('/test', (req, res) => {
     const response={
@@ -47,6 +53,37 @@ else {
 res.send(response);
 }
 })
+
+app.get('/movies/create', (req, res) => {
+    const response={
+        status:200, message:"OK"
+    };
+    res.send(response);
+})
+
+app.get('/movies/read', (req, res) => {
+    
+    const response={
+        status:200, data: movies
+    };
+    res.send(response);
+})
+
+app.get('/movies/update', (req, res) => {
+    const response={
+        status:200, message:"OK"
+    };
+    res.send(response);
+})
+
+app.get('/movies/delete', (req, res) => {
+    const response={
+        status:200, message:"OK"
+    };
+    res.send(response);
+})
+
+
 app.listen(port, () => {
   console.log('server listening on port http://localhost:${port}')
 })
