584a631c992085fca86ed55f71666cc4243bcd21
muhammad-jamal-bek <muhammad-jamal-bek@hotmail.com>
2021-06-14 11:01:55 +0300

    Step 3

--- index.js
+++ index.js
@@ -2,8 +2,19 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('OK')
+app.get('/test', (req, res) => {
+    const response={
+        status:200, message:"OK"
+    };
+    res.send(response)
+})
+
+app.get('/time', (req, res) => {
+    var date= new Date()
+    const response={
+        status:200, message: date.getHours()+":"+date.getMinutes()
+    };
+    res.send(response)
 })
 
 app.listen(port, () => {
